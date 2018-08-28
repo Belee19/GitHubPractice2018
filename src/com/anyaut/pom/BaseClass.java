91a767a475821d0b8229fb170099c01e87b89eb3
Belee <beleebd@yahoo.com>
2018-08-27 20:00:16 -0400

    Full Project

---
+++ src/com/anyaut/pom/BaseClass.java
@@ -0,0 +1,40 @@
+package com.anyaut.pom;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	private static WebDriver driver;
+	private static String baseUrl;
+	static SignInPageFactory signInPageFactory;
+
+	@BeforeClass
+	public static void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", "G:\\Aumation Software\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		baseUrl = "https://anyaut.com/orange/index.php";
+		signInPageFactory = new SignInPageFactory(driver);
+		driver.manage().window().maximize();
+		driver.get(baseUrl);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void test() {
+		signInPageFactory.enterUserName("example.com");
+		signInPageFactory.enterPassword("XYZ123");
+		signInPageFactory.clickEyeIcon();
+		signInPageFactory.clickSignIn();
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		driver.quit();
+	}
+
+}
---
+++ src/com/anyaut/pom/SignInPageFactory.java
@@ -0,0 +1,45 @@
+package com.anyaut.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignInPageFactory {
+	WebDriver driver;
+
+	@FindBy(id = "username")
+	WebElement userName;
+
+	@FindBy(id = "password")
+	WebElement password;
+
+	@FindBy(xpath = "//span[@class='soh']")
+	WebElement eyeIcon;
+
+	@FindBy(name = "submit")
+	WebElement signIn;
+
+	public SignInPageFactory(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public void enterUserName(String userId) {
+		userName.sendKeys(userId);
+	}
+
+	public void enterPassword(String pass) {
+		password.sendKeys(pass);
+	}
+
+	public void clickEyeIcon() {
+		eyeIcon.click();
+	}
+
+	public void clickSignIn() {
+		signIn.click();
+	}
+
+}
